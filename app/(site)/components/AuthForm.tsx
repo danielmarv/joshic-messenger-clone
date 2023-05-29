58a8dff936ed140ceb68c0ea096717158b8a175a
danielmarv <103020028+danielmarv@users.noreply.github.com>
2023-05-29 20:23:32 +0300

    Authentication form

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,4 +1,20 @@
+'use client';
+
+import { useCallback, useState } from "react";
+
+type Variant = 'LOGIN' | 'REGISTER';
+
 const AuthForm = () => {
+    const [variant, setVariant] = useState<Variant>('LOGIN');
+    const [isLording, setIsLording] = useState(false);
+
+    const toggleVariant = useCallback(() => {
+        if (variant === 'LOGIN') {
+            setVariant('REGISTER');
+        } else {
+            setVariant('LOGIN');
+        }
+    }, [variant]);
   return (
     <div>AuthForm</div>
   )
