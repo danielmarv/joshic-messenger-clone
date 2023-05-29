f809fc74a98208986b830cb8c974c21d55ab30a1
danielmarv <103020028+danielmarv@users.noreply.github.com>
2023-05-29 22:08:20 +0300

    Commit

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,6 +1,12 @@
 'use client';
 
+import Input from "@/app/components/inputs/Input";
 import { useCallback, useState } from "react";
+import { 
+    FieldValues, 
+    SubmitHandler, 
+    useForm 
+} from "react-hook-form";
 
 type Variant = 'LOGIN' | 'REGISTER';
 
@@ -15,9 +21,55 @@ const AuthForm = () => {
             setVariant('LOGIN');
         }
     }, [variant]);
-  return (
-    <div>AuthForm</div>
-  )
+
+    const {
+        register,
+        handleSubmit,
+        formState: {
+            errors
+        }
+    } = useForm<FieldValues>({
+        defaultValues: {
+            name: '',
+            email: '',
+            password: ''
+        }
+    });
+
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+        setIsLording(true);
+
+        if (variant === 'REGISTER') {
+            //Axios
+        }
+
+        if (variant === 'LOGIN'){
+            // NextAuth SignIn
+        }
+
+        const socialAction = (action: string) => {
+
+        }
+    }
+    return (
+        <div 
+            className="
+                bg-white
+                px-4
+                py-8
+                shadow
+                sm:rounded-lg
+                sm:px-10
+            "
+        >
+            <form
+                className="space-y-6"
+                onSubmit={handleSubmit(onSubmit)}
+            >
+                <Input />
+            </form>
+        </div>
+    )
 }
 
 export default AuthForm;
