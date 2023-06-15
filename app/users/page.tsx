b88b6b9798018043bca7f0ecf0ac16f8c4dbee39
danielmarv <103020028+danielmarv@users.noreply.github.com>
2023-06-15 22:16:32 +0300

    Credentials

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 import { signOut } from "next-auth/react"
+import EmptyState from "../components/EmptyState";
 
 const Users = () => {
     return (
-        <button onClick={() => signOut()}>
-            Logout
-        </button>
+        <div className="hidden lg:block lg:pl-80 h-full">
+            <EmptyState />
+        </div>
     )
 }
 
